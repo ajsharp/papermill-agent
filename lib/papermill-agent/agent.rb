8cb0a5679f4302fac73ca3f4c0665c76901986f9
Alex Sharp <ajsharp@gmail.com>
2010-11-12 14:23:32 -0600

    close to working agent

--- lib/papermill-agent/agent.rb
+++ lib/papermill-agent/agent.rb
@@ -1,27 +1,64 @@
 require 'singleton'
 require 'timeout'
+require 'json'
+
+# 13 on 517 frenchman st
 
 module Papermill
 
   class Agent
     include Singleton
+
+    # papermill endpoint which will receive client requests
+    API_ENDPOINT = 'http://api.papermillapp.com'
     
     # send new request data every 10 seconds
     UPDATE_INTERVAL = 10
 
-    def start
-      connect!
+    attr_reader :last_sent
 
+    def mutex
+      @mutex ||= Mutex.new
+    end
+
+    def config
+      @config ||= YAML.load_file('config/papermill.yml')
+    end
+
+    def start
+      @last_sent = Time.now
       @worker_thread = Thread.new do
-        while should_run? do
-          # start collecting request data
+        loop do
+          if time_since_last_sent > 10
+            send_data_to_papermill
+            @last_sent = Time.now
+          end
         end
       end
     end
 
-    def connect!
+    def time_since_last_sent
+      Time.now - last_sent
+    end
 
-    end    
+    def send_data_to_papermill
+      Timeout.timeout(UPDATE_INTERVAL - 1) do
+        api_key = config['token']
+        RestClient.post API_ENDPOINT, { :api_key => api_key, :payload => jsonify_payload }
+      end
+    end
+
+    # Return a json version of the storage array.
+    # Also, we'll clear out the storage here
+    #
+    # TODO: move this to the Storage class.
+    def jsonify_payload
+      mutex.synchronize do
+        json_data = JSON.generate(Storage.store.flatten)
+        Storage.clear
+        return json_data
+      end
+    end
   end
 
 end
--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -1,40 +1,77 @@
 require 'spec_helper'
 
-describe Papermill::Agent, 'intializing the agent' do
-  context 'when a new process is started' do
-    context 'when an agent process does not already exist' do
-      it 'should create a new agent process' do
-        Papermill::Agent.instance.should be_instance_of Papermill::Agent
+module Papermill
+
+  describe 'the agent' do
+    it 'has a mutex' do
+      Agent.instance.mutex.should be_instance_of Mutex
+    end
+
+    context 'when a new process is started' do
+      context 'when an agent process does not already exist' do
+        it 'should create a new agent process' do
+          Papermill::Agent.instance.should be_instance_of Papermill::Agent
+        end
+      end
+
+      context 'when an agent process already exists' do
+        it 'should only allow one agent instance to exist' do
+          Papermill::Agent.instance.should eql Papermill::Agent.instance
+        end
       end
     end
 
-    context 'when an agent process already exists' do
-      it 'should only allow one agent instance to exist' do
-        Papermill::Agent.instance.should eql Papermill::Agent.instance
+    context 'when attempting to initialize the agent directly' do
+      it 'raises a NoMethodError for attempting to initialize a singleton' do
+        lambda {
+          Papermill::Agent.initialize
+        }.should raise_error(NoMethodError, /private method `initialize' called/)
       end
     end
+
   end
 
-  context 'when attempting to initialize the agent directly' do
-    it 'raises a NoMethodError for attempting to access a private method' do
-      lambda {
-        Papermill::Agent.initialize
-      }.should raise_error(NoMethodError, /private method `initialize' called/)
+  describe 'the api endpoint' do
+    subject { Agent::API_ENDPOINT }
+    it { should == 'http://api.papermillapp.com' }
+  end
+
+  describe 'determining the time since the last time data was sent to papermill' do
+    before do
+      Agent.instance.stub!(:last_sent => Time.mktime(2010, 11, 11, 0, 0, 0))
+      Time.stub!(:now => Time.mktime(2010, 11, 11, 0, 0, 9))
+    end
+
+    it 'subtracts the current time from @last_sent' do
+      Agent.instance.time_since_last_sent.should == 9
+    end
+  end
+
+  describe 'configuration' do
+    it 'provides access to the api token' do
+      Agent.instance.config['token'].should == 'api-key'
+    end
+  end
+
+  describe 'sending data to papermill' do
+    before do
+      Storage.store << [{:headers => {'Content-Type' => 'text/html'}, :status => 200}]
+    end
+
+    it 'jsonifies the payload data' do
+      Agent.instance.jsonify_payload.should == '[{"headers":{"Content-Type":"text/html"},"status":200}]'
+    end
+
+    it 'empties the store' do
+      Agent.instance.jsonify_payload
+      Storage.store.should be_empty
+    end
+
+    it 'sends a request to the papermill api endpoint' do
+      RestClient.should_receive(:post).with(Agent::API_ENDPOINT, :api_key => 'api-key', :payload => '[{"headers":{"Content-Type":"text/html"},"status":200}]')
+      Agent.instance.send_data_to_papermill
     end
   end
 
 end
 
-describe Papermill::Agent, '#start' do
-  it 'attempts to contact the server' do
-    Papermill::Agent.instance.should_receive(:connect!)
-    Papermill::Agent.instance.start
-  end
-end
-
-describe 'connecting to the papermill server' do
-  context 'when we should connect to a remote'
-end
-
-describe 'determining if we should connect to the remote papermill server' do
-end
