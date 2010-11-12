fb2b0ea339ce9a7c4f2267e33bd4b0e5de8bd80a
Alex Sharp <ajsharp@gmail.com>
2010-11-12 11:41:06 -0600

    add autotest

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'rubygems'
+require 'bundler'
+
+Bundler.require :default, :test
+
 $:.unshift File.dirname(File.expand_path(__FILE__))
 $:.unshift File.dirname(File.expand_path(__FILE__) + '/../lib')
 
 require 'papermill-agent'
+
+RSpec.configure do |config|
+
+end
