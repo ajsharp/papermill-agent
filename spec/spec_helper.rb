701dbb029aee0dc3b5c030b69dc68d5d1a67491e
Alex Sharp <ajsharp@gmail.com>
2010-11-12 14:36:41 -0600

    clear storage out before each example

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,13 @@ $:.unshift File.dirname(File.expand_path(__FILE__))
 $:.unshift File.dirname(File.expand_path(__FILE__) + '/../lib')
 
 require 'papermill-agent'
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
 
 RSpec.configure do |config|
-
+  config.before(:each) do
+    # clear the storage before each example
+    Papermill::Storage.clear
+  end
 end
