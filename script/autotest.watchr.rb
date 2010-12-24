5ece52b8137f06e6041df94c9f31438bfa21ff2b
Alex Sharp <ajsharp@gmail.com>
2010-12-24 15:44:50 -0800

    refining watchr script

--- script/autotest.watchr.rb
+++ script/autotest.watchr.rb
@@ -11,13 +11,15 @@ def run_spec(spec)
     system "clear"
     puts "Running #{spec}"
     system(cmd + spec)
-    puts
+  else
+    puts "Cannot find spec file #{spec}"
   end
 end
 
 watch('^spec\/.*\/.*_spec\.rb') {|md| run_spec(md[0]) }
 watch('^spec\/.*_spec\.rb') {|md| run_spec(md[0]) }
-watch('^lib/(.*)\.rb') {|md| run_spec("spec/#{md[1]}_spec.rb") }
+watch('^lib/.*\.rb') {|md| run_spec("spec/#{md[1]}_spec.rb") }
+watch('^lib/.*\/(.*)\.rb') {|md| run_spec("spec/#{md[1]}_spec.rb") }
 
 # Ctrl-\
 Signal.trap('QUIT') do
