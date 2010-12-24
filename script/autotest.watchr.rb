b6694511859841b962a8f51ff675468bcb93502f
Alex Sharp <ajsharp@gmail.com>
2010-12-24 13:52:01 -0800

    add autotest watchr script for when autotest is on the fritz

---
+++ script/autotest.watchr.rb
@@ -0,0 +1,29 @@
+puts "\n### Watching specs###\n"
+ 
+def cmd() 'bundle exec rspec '; end
+ 
+def run_all_specs
+  system(cmd + 'spec/')
+end
+ 
+def run_spec(spec)
+  if File.exists?(spec)
+    system "clear"
+    puts "Running #{spec}"
+    system(cmd + spec)
+    puts
+  end
+end
+
+watch('^spec\/.*\/.*_spec\.rb') {|md| run_spec(md[0]) }
+watch('^spec\/.*_spec\.rb') {|md| run_spec(md[0]) }
+watch('^lib/(.*)\.rb') {|md| run_spec("spec/#{md[1]}_spec.rb") }
+
+# Ctrl-\
+Signal.trap('QUIT') do
+  puts "\n### Running all specs###\n"
+  run_all_specs
+end
+       
+# Ctrl-C
+Signal.trap('INT') { abort("\n") }
