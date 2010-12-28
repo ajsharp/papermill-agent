d4b5a4ad9b212979618b6aafcd49b553b1e769d5
Alex Sharp <ajsharp@gmail.com>
2010-12-28 07:09:37 -0600

    add mock_config_hash method to test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,10 @@ FakeWeb.allow_net_connect = false
 FakeWeb.register_uri(:post, Papermill::API_ENDPOINT, {})
 
 RSpec.configure do |config|
+  def mock_config_hash
+    {'token' => 'mock-token'}
+  end
+
   def agent
     Papermill::Agent.instance
   end
