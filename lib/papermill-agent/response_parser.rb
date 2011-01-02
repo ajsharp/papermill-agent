f11168282826231547f1e069ceecba677b482d34
Alex Sharp <ajsharp@gmail.com>
2011-01-02 01:28:54 -0600

    memoize the response parser class in a class-level ivar

--- lib/papermill-agent/response_parser.rb
+++ lib/papermill-agent/response_parser.rb
@@ -2,22 +2,18 @@
 module Papermill
 
   class ResponseParser
-    # ENV_CAPTURE_FIELDS = [
-    #   'PATH_INFO', 'QUERY_STRING', 'REMOTE_ADDR', 'REMOTE_HOST', 'REQUEST_METHOD', 
-    #   'REQUEST_URI', 'SCRIPT_NAME', 'SERVER_NAME', 'SERVER_SOFTWARE', 'HTTP_HOST', 
-    #   'HTTP_ACCEPT', 'HTTP_USER_AGENT', 'REQUEST_PATH'
-    # ]
-
-    def self.parse(status, headers, response, env = {})
-      klass = if defined?(Rails)
+    def self.parse_klass
+      @klass ||= if defined?(Rails)
         ResponseAdapters::Rails
       elsif defined?(Sinatra)
         ResponseAdapters::Sinatra
       else
         ResponseAdapters::Base
       end
+    end
 
-      klass.new(status, headers, response, env).parse
+    def self.parse(status, headers, response, env = {})
+      parse_klass.new(status, headers, response, env).parse
     end
   end
 
